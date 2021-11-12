910bbbfa1d5852297bc1b3a1ac4c40830890b5b4
Lucchetto <lucchetto.tie@live.com>
2021-11-12 11:52:20 +0100

    sdm660-common: Import datastatusnotification from coral [2/2]
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Change-Id: Ied78261148640f9f4c7862ca6e86ff809be3bfe3

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -641,6 +641,7 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
+    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
